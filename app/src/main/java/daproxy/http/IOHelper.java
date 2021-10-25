328912febbfb796ffda6df737a84565a5b2e039c
Ian <ian@logicalturn.com>
2021-10-24 20:12:38 -0400

    Refactor: clean-up ConnectRequest and extract IOHelper for reading/writing from input to output

---
+++ app/src/main/java/daproxy/http/IOHelper.java
@@ -0,0 +1,35 @@
+package daproxy.http;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.SocketException;
+
+//import daproxy.log.LogUtils;
+
+public class IOHelper {
+
+    private static final int BUFFER_SIZE = 1024;
+    private static final int END_OF_STREAM = -1;
+
+    /**
+     * A helper method to write bytes from an InputStream to an OutputStream, and throw an exception once detecting the socket is closed.
+     * @param in
+     * @param out
+     * @throws IOException
+     */
+    public static void writeInputToOutput(InputStream in, OutputStream out) throws IOException {
+        int recvBytes;
+        byte[] buf = new byte[BUFFER_SIZE];
+
+        while ((recvBytes = in.read(buf)) != END_OF_STREAM) {
+            // System.out.println("Thread: Writing " + recvBytes + " to socket");
+            // System.out.println("HEX Output: " + LogUtils.bytesToHex(buf, 0, recvBytes));
+            out.write(buf, 0, recvBytes);
+        }
+
+        if (recvBytes == END_OF_STREAM) { // socket is closed
+            throw new SocketException("Detected closed socket");
+        }
+    }
+}
--- app/src/main/java/daproxy/http/request/ConnectRequest.java
+++ app/src/main/java/daproxy/http/request/ConnectRequest.java
@@ -1,22 +1,18 @@
 package daproxy.http.request;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.Socket;
-import java.net.SocketException;
 
+import daproxy.http.IOHelper;
 import daproxy.http.RequestMethod;
 import daproxy.http.Response;
 import daproxy.http.Url;
-import daproxy.log.LogUtils;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * A valid CONNECT request is as follows:
- * CONNECT SP <url> SP HTTP/1.1 CRLF
- * where SP is space, and CRLF is \r\n
- * See: https://datatracker.ietf.org/doc/html/rfc2616/ [page 16]
+ * A valid CONNECT request is as follows: CONNECT SP <url> SP HTTP/1.1 CRLF
+ * where SP is space, and CRLF is \r\n See:
+ * https://datatracker.ietf.org/doc/html/rfc2616/ [page 16]
  */
 @Slf4j
 public class ConnectRequest implements Request {
@@ -36,30 +32,11 @@ public class ConnectRequest implements Request {
         return RequestMethod.CONNECT;
     }
 
-
-
-    private void writeInputToOutput(InputStream in, OutputStream out) throws IOException {
-        int recvBytes;
-        byte[] buf = new byte[1024];
-
-        while ((recvBytes = in.read(buf)) != -1) {
-            System.out.println("Thread: Writing " + recvBytes + " to socket");
-            
-            System.out.println("HEX Output: " + LogUtils.bytesToHex(buf, 0, recvBytes));
-            out.write(buf, 0, recvBytes);
-        }
-
-        if (recvBytes == -1) { //socket is closed
-            throw new SocketException("Detected closed socket");
-        }
-
-    }
-
-
-
     @Override
     public Response handle(Socket socket) {
-        System.out.println("Handling a Connect Request");
+        log.debug("Handling a Connect Request to url: " + url);
+        log.debug("Did not handle headers: " + headersBlob);
+        log.debug("Did not write possible first bytes: " + firstBytesToWrite);
         try {
             Socket downstreamSocket = new Socket(url.getHost(), url.getPort()); // extractPort());
             // if this connects, we can give a 200 OK back to client, which will then allow
@@ -69,75 +46,51 @@ public class ConnectRequest implements Request {
             Response resp = new Response(200);
 
             socket.getOutputStream().write(resp.toString().getBytes());
-
-            System.out.println("writing " + resp.toString() + " to client socket");
-            // br.flush();
             socket.getOutputStream().flush();
-            // br.close();
+            log.debug("Wrote " + resp.toString() + " to client socket to indicate successful connect to downstream");
 
             // read from client, write to server. read from remote, write to client. repeat.
 
             new Thread(() -> {
-                while (!socket.isClosed() && !downstreamSocket.isClosed()) {
-                    try {
-                        Thread.sleep(500);
-                        System.out.println("Thread: Socket (bound, connected, closed, in avail): (" + socket.isBound()
-                                + ", " + socket.isConnected() + ", " + socket.isClosed() + ", "
-                                + socket.getInputStream().available() + ")");
-                        System.out.println("Thread: Downstream Socket (bound, connected, closed, in avail): ("
-                                + downstreamSocket.isBound() + ", " + downstreamSocket.isConnected() + ", "
-                                + downstreamSocket.isClosed() + ", " + downstreamSocket.getInputStream().available()
-                                + ")");
-
-                        writeInputToOutput(socket.getInputStream(), downstreamSocket.getOutputStream());
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                        try {
-                            socket.close();
-                        } catch (Exception ex2) {
-                            ex.printStackTrace();
-                        }
-                        try {
-                            downstreamSocket.close();
-                        } catch (Exception ex2) {
-                            ex.printStackTrace();
-                        }
-                    }
-                } //TODO: Close all sockets.
+                writeAllData(socket, downstreamSocket);
 
             }).start();
 
-            while (!socket.isClosed() && !downstreamSocket.isClosed()) { // ugh, this is not what you want: https://stackoverflow.com/questions/10240694/java-socket-api-how-to-tell-if-a-connection-has-been-closed/10241044
-                try {
-                    Thread.sleep(500);
-                    System.out.println("Main: Socket (bound, connected, closed, in avail): (" + socket.isBound() + ", "
-                            + socket.isConnected() + ", " + socket.isClosed() + ", "
-                            + socket.getInputStream().available() + ")");
-                    System.out.println("Main: Downstream Socket (bound, connected, closed, in avail): ("
-                            + downstreamSocket.isBound() + ", " + downstreamSocket.isConnected() + ", "
-                            + downstreamSocket.isClosed() + ", " + downstreamSocket.getInputStream().available() + ")");
+            writeAllData(downstreamSocket, socket);
 
-                    writeInputToOutput(downstreamSocket.getInputStream(), socket.getOutputStream());
-
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                    try {
-                        socket.close();
-                    } catch (Exception ex2) {
-                        ex.printStackTrace();
-                    }
-                    try {
-                        downstreamSocket.close();
-                    } catch (Exception ex2) {
-                        ex.printStackTrace();
-                    }
-                }
-
-            } //TODO: Close all sockets.
         } catch (Exception ex) {
             ex.printStackTrace();
         }
 
         return Response.CONNECTION_ESTABLISHED();
+    }
+
+
+    public void writeAllData(Socket input, Socket output) {
+        while (!input.isClosed() && !output.isClosed()) {
+            try {
+                // log.debug("Thread: Socket (bound, connected, closed, in avail): (" + input.isBound()
+                //         + ", " + input.isConnected() + ", " + input.isClosed() + ", "
+                //         + input.getInputStream().available() + ")");
+                // log.debug("Thread: Downstream Socket (bound, connected, closed, in avail): ("
+                //         + output.isBound() + ", " + output.isConnected() + ", "
+                //         + output.isClosed() + ", " + output.getInputStream().available()
+                //         + ")");
+
+                IOHelper.writeInputToOutput(input.getInputStream(), output.getOutputStream());
+            } catch (IOException ex) {
+                log.debug("Caught IOException - usually means connection is closed", ex);
+                try {
+                    input.close();
+                } catch (Exception ex2) {
+                    log.error("Error trying to close input socket", ex2);
+                }
+                try {
+                    output.close();
+                } catch (Exception ex2) {
+                    log.error("Error trying to close output socket", ex2);
+                }
+            }
+        } 
     }
 }
